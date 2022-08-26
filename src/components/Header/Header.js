f35d0f88811dad985c2cbaff844e3784fd7f36f2
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-26 10:27:40 +0600

    nav bar add

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav>
+            <ul className=''>
+                <li><a href="/home">Home</a></li>
+                <li><a href="/shop">Shop</a></li>
+                <li><a href="/men">Men</a></li>
+                <li><a href="/women">Women</a></li>
+                <li><a href="/chidlren">Children</a></li>
+            </ul>
+
+        </nav>
+    );
+};
+
+export default Header;
